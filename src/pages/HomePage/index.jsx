1dab0f0db008662cd662828f1d94831e2d93a021
Linda Hrdinová <hrlin@seznam.cz>
2023-11-25 15:13:42 +0100

    jokes.map funguje

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,58 +1,36 @@
 import { Jokes } from '../../components/Jokes';
 import './style.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const HomePage = () => {
-  const [like, setLike] = useState(0);
-  const [dislike, setDislike] = useState(0);
+  const [likes, setLike] = useState(0);
+  const [dislikes, setDislike] = useState(0);
+  const [jokesList, setJokesList] = useState([]);
 
-  const fetchJokes = async () => {
-    const response = await fetch(
-      'https://raw.githubusercontent.com/Czechitas-podklady-WEB/daweb-test/deploy/jokes.json',
-    );
-    const data = await response.json();
-    jokes(data.result);
-  };
+  useEffect(() => {
+    const fetchJokes = async () => {
+      const response = await fetch(
+        'https://raw.githubusercontent.com/Czechitas-podklady-WEB/daweb-test/deploy/jokes.json',
+      );
+      const data = await response.json();
+      setJokesList(data);
+    };
 
-  fetchJokes();
+    fetchJokes();
+  }, []);
 
   return (
     <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img
-              className="user-avatar"
-              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-            />
-            <p className="user-name">Neroxx</p>
-          </div>
-
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-        <div className="joke__likes">
-          <button
-            id="btn-up"
-            className="btn-like btn-like--up"
-            onClick={() => setLike(like + 1)}
-          ></button>
-          <span id="likes-up" className="likes-count likes-count--up">
-            {like}
-          </span>
-          <button
-            id="btn-down"
-            className="btn-like btn-like--down"
-            onClick={() => setDislike(dislike + 1)}
-          ></button>
-          <span id="likes-down" className="likes-count likes-count--down">
-            {dislike}
-          </span>
-        </div>
-      </div>
+      {jokesList.map((joke) => (
+        <Jokes
+          key={joke.id}
+          userAvatar={joke.avatar}
+          userName={joke.name}
+          text={joke.text}
+          likes={joke.likes}
+          dislikes={joke.dislikes}
+        />
+      ))}
     </div>
   );
 };
